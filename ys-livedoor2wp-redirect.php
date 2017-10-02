f34b44821e987a3b11fa7821ce51e96278ede1ec
yosiakatsuki <y.ogata@yosiakatsuki.net>
2017-10-02 11:14:18 +0900

    first commit

---
+++ ys-livedoor2wp-redirect.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name:     YS livedoor2wp redirect
+ * Description:     ライブドアブログ時代のURLを上手いことWordPress時代のURLにリダイレクトするプラグイン
+ * Author:          yosiakatsuki
+ * Author URI:      https://yosiakatsuki.net/
+ * Version:         1.0.0
+ *
+ * @package         ys-livedoor2wp-redirect
+ */
+
+/*
+	Copyright (c) 2016 Yoshiaki Ogata (https://yosiakatsuki.net/)
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/

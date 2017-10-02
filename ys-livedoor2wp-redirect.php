de015557e9073987682e6cde88995a5d150e2d32
yosiakatsuki <y.ogata@yosiakatsuki.net>
2017-10-02 14:08:20 +0900

    リダイレクト履歴を見れるようにした

--- ys-livedoor2wp-redirect.php
+++ ys-livedoor2wp-redirect.php
@@ -60,3 +60,88 @@ function l2wpr_redirect() {
   }
 }
 add_action( 'template_redirect', 'l2wpr_redirect' );
+
+function l2wpr_admin_menu() {
+	add_options_page(
+		'ライブドアブログ->WP',
+		'ライブドアブログ->WP',
+		'manage_options',
+		'ys-livedoor2wp-redirect',
+		'l2wpr_options_page'
+	);
+}
+add_action( 'admin_menu', 'l2wpr_admin_menu' );
+
+function l2wpr_options_page() {
+if ( ! current_user_can( 'manage_options' ) ) {
+		wp_die( __( 'You do not have sufficient permissions to access this page.', 'ys-simple-google-analytics' ) );
+	}
+	?>
+
+	<div class="wrap">
+		<h2>最近のリダイレクト履歴(簡易版)</h2>
+		<div id="poststuff" class="l2wpr_options_page">
+
+			<?php
+				$arg = array(
+								'meta_key'=>'l2wpr_redirect_last_time',
+								'meta_value'=>'1900/01/01 00:00:00',
+								'meta_compare'=>'>',
+								'order'=>'DESC',
+								'orderby'=>'meta_value'
+							);
+				$posts = get_posts($arg);
+				if( $posts ): ?>
+					<table>
+						<tbody>
+							<tr>
+								<th class="last-time">最終リダイレクト</th>
+								<th class="count">累計回数</th>
+								<th class="title">タイトル</th>
+							</tr>
+					<?php
+						foreach ($posts as $post) :
+							$post_id = $post->ID;
+							?>
+							<tr>
+								<td><?php echo esc_html( get_post_meta( $post_id, 'l2wpr_redirect_last_time', true ) ); ?></td>
+								<td><?php echo esc_html( get_post_meta( $post_id, 'l2wpr_redirect_cnt', true ) ); ?></td>
+								<td><?php echo esc_html( get_the_title( $post_id ) ); ?></td>
+							</tr>
+					<?php
+						endforeach; ?>
+					</tbody>
+				</table>
+			<?php
+				endif;
+			 ?>
+		</div>
+	</div><!-- /.warp -->
+	<style>
+	.l2wpr_options_page table {
+		width: 100%;
+		table-layout: fixed;
+		border-top: 1px solid #333;
+		border-left: 1px solid #333;
+		box-sizing: border-box;
+		padding: 0;
+		margin: 0;
+		    border-spacing:0;
+	}
+		.l2wpr_options_page th ,
+		.l2wpr_options_page td {
+			padding: .5em;
+			margin: 0;
+			border-right: 1px solid #333;
+			border-bottom: 1px solid #333;
+			box-sizing: border-box;
+		}
+		.l2wpr_options_page table .last-time {
+			width: 200px;
+		}
+		.l2wpr_options_page table .count {
+			width: 100px;
+		}
+	</style>
+<?php
+}

ede3be617d53525d93fd6a8de8f8646344a874a9
yosiakatsuki <y.ogata@yosiakatsuki.net>
2017-10-02 13:21:06 +0900

    リダイレクトログ収集機能追加

--- ys-livedoor2wp-redirect.php
+++ ys-livedoor2wp-redirect.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:     YS livedoor2wp redirect
- * Description:     ライブドアブログ時代のURLを上手いことWordPress時代のURLにリダイレクトするプラグイン
+ * Description:     ライブドアブログ時代のURLを上手いことWordPress時代のURLにリダイレクトするプラグイン（暫定的に）
  * Author:          yosiakatsuki
  * Author URI:      https://yosiakatsuki.net/
  * Version:         1.0.0
@@ -22,3 +22,41 @@
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+function l2wpr_redirect() {
+	// 404の時にライブドアブログの時のURLか判断してリダイレクトかける
+  if( is_404() ){
+
+		$request_url = $_SERVER["REQUEST_URI"];
+		$url = home_url();
+
+		// パーマリンク設定に.htmlを入れるとライブドアブログ時代以外でも処理してしまう
+		if ( strpos( $request_url, '.html' ) !== FALSE){
+
+			$temp = substr( $request_url, strrpos($request_url, '/') + 1);
+			$temp = str_replace( '.html', '', $temp );
+			$post = get_page_by_path( $temp, OBJECT, 'post' );
+			if( $post ){
+				$post_id = $post->ID;
+				$redirect_url = get_permalink( $post_id );
+
+				$key_hit = 'l2wpr_redirect_cnt';
+				$val_hit = get_post_meta( $post_id, $key_hit, true );
+				if( ! $val_hit || ! is_numeric( $val_hit ) ){
+					$val_hit = 0;
+				} else {
+					$val_hit = (int)$val_hit;
+				}
+				$val_hit ++;
+
+				update_post_meta( $post_id, $key_hit, $val_hit );
+				update_post_meta( $post_id, 'l2wpr_redirect_last_time', date_i18n( 'Y/m/d H:i:s' ) );
+
+				// リダイレクト
+				wp_safe_redirect( $redirect_url, 301 );
+				exit();
+			}
+
+		}
+  }
+}
+add_action( 'template_redirect', 'l2wpr_redirect' );
